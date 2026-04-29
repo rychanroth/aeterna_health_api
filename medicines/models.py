8354a63cf635616322ad8aaf30c655ddb8ccf885
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-29 20:46:39 +0700

    feat(model): add Medicine

--- medicines/models.py
+++ medicines/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Medicine(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
