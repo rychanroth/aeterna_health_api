acf98d2d38b4d85050d3bf1b33dc0efe321c83a2
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-29 21:06:16 +0700

    feat(view): add medicine_list()

--- medicines/views.py
+++ medicines/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET'])
+def medicine_list(request):
+    """List all medicines."""
+    medicines = Medicine.objects.all().order_by('created_at')
+    serializer = MedicineSerializer(medicines, many=True)
+    return  Response(serializer.data)
