6b95fd65e894f334b986f7c4bc2cbce039a0e3a6
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-30 18:21:05 +0700

    feat(view): add put, patch, delete to medicine_detail

--- medicines/views.py
+++ medicines/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 # Create your views here.
 @api_view(['GET', 'POST'])
 def medicine_list(request):
-    """Get or create data."""
+    """List all medicines, or create a new medicine."""
     if request.method == 'GET':
         medicines = Medicine.objects.all().order_by('created_at')
         serializer = MedicineSerializer(medicines, many=True)
@@ -21,10 +21,34 @@ def medicine_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-
-
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def medicine_detail(request, pk):
-    medicine = Medicine.objects.get(pk=pk)
-    serializer = MedicineSerializer(medicine)
-    return Response(serializer.data)
+    """Retrieve medicines, then put, patch, or delete medicine."""
+    # Find or fail
+    try:
+        medicine = Medicine.objects.get(pk=pk)
+    except Medicine.DoesNotExist:
+        return Response({'error': 'Medicine does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+
+    if request.method == 'GET':
+        serializer = MedicineSerializer(medicine)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = MedicineSerializer(medicine, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'PATCH':
+        serializer = MedicineSerializer(medicine, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        medicine.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
