22326be6ac3e0cf9dd879fbef73919fcc563d0e9
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-30 18:34:39 +0700

    feat(model): add Category and foreigned category on Medicine

--- medicines/models.py
+++ medicines/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories' # Fix admin pluralization
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
 class Medicine(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='medicines'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
