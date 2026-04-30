d0e743338826fe0843f6641c5aa143f4139b0735
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-30 10:23:23 +0700

    feat(url): add medicine-detail

--- medicines/urls.py
+++ medicines/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('medicines/', views.medicine_list, name='medicine-list')
+    path('medicines/', views.medicine_list, name='medicine-list'),
+    path('medicines/<int:pk>/', views.medicine_detail, name='medicine-detail')
 ]
