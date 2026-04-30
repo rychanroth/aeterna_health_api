512c2dbad101f141694b39c80d7f53acbe67087e
Ry Chanroth <rychanroth2009@gmail.com>
2026-04-30 09:10:48 +0700

    feat(view): add post request to medicine_list

--- medicines/views.py
+++ medicines/views.py
@@ -6,12 +6,22 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def medicine_list(request):
-    """List all medicines."""
-    medicines = Medicine.objects.all().order_by('created_at')
-    serializer = MedicineSerializer(medicines, many=True)
-    return  Response(serializer.data)
+    """Get or create data."""
+    if request.method == 'GET':
+        medicines = Medicine.objects.all().order_by('created_at')
+        serializer = MedicineSerializer(medicines, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MedicineSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
 
 @api_view(['GET'])
 def medicine_detail(request, pk):
