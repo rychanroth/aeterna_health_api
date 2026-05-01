1963332df36b59d0793d52171513d6e65ed21a5c
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-01 15:22:37 +0700

    feat(view): add category_list and category_detail

--- medicines/views.py
+++ medicines/views.py
@@ -6,6 +6,55 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
+
+# === CATEGORIES VIEWS ===
+@api_view(['GET', 'POST'])
+def category_list(request):
+    """List all categories."""
+    if request.method == 'GET':
+        categories = Category.objects.all().order_by('name')
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def category_detail(request, pk):
+    """Retrieve categories, then put, patch or delete the category."""
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        return Response({'error': 'Category does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'PATCH':
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# === MEDICINES VIEWS  === 
 @api_view(['GET', 'POST'])
 def medicine_list(request):
     """List all medicines, or create a new medicine."""
