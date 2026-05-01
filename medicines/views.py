a9430a6f544a99600b6c3989bfe016599bff9a62
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-01 19:17:59 +0700

    feat(view): add action roots to CategoryViewSet

--- medicines/views.py
+++ medicines/views.py
@@ -1,12 +1,23 @@
 from .models import *
 from .serializers import *
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # ModelViewSet automatically provides list(), create(), retrieve(), update(), partial_update(), destroy()
 # When registered with Router, it dynamically generates the URL PATTERNS!
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    # Custom Action
+    # Function name determines the auto-generated url name e.g. /api/categories/roots/
+    @action(detail=False, methods=['get'])
+    def roots(self, request):
+        """Get all root categories (category with no parent)"""
+        root_categories = self.queryset.filter(parent__isnull=True)
+        serializer = self.get_serializer(root_categories, many=True)
+        return Response(serializer.data)
 
 class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
