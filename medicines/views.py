fa89f989688fe0ca22284b1c10471f550ed2319d
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-01 19:30:03 +0700

    feat(view): add action medicines to CategoryViewSet

--- medicines/views.py
+++ medicines/views.py
@@ -10,13 +10,21 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    # Custom Action
+    # === Custom Action ===
     # Function name determines the auto-generated url name e.g. /api/categories/roots/
     @action(detail=False, methods=['get'])
     def roots(self, request):
         """Get all root categories (category with no parent)"""
         root_categories = self.queryset.filter(parent__isnull=True)
         serializer = self.get_serializer(root_categories, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'])
+    def medicines(self, request, pk=None):
+        """Get all medicines in this category"""
+        category = self.get_object()
+        medicines = category.medicines.all()
+        serializer = MedicineSerializer(medicines, many=True)
         return Response(serializer.data)
 
 class SupplierViewSet(viewsets.ModelViewSet):
