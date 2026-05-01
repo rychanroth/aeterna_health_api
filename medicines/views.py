a6eb5b42dc36f6f3685b24a5f99148d8616d7a60
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-01 19:06:05 +0700

    refactor(view): change from function-based to viewset

--- medicines/views.py
+++ medicines/views.py
@@ -1,155 +1,17 @@
-from django.shortcuts import render
 from .models import *
 from .serializers import *
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
 
-# Create your views here.
+# ModelViewSet automatically provides list(), create(), retrieve(), update(), partial_update(), destroy()
+# With the exception of using Router of course
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-# === CATEGORIES VIEWS ===
-@api_view(['GET', 'POST'])
-def category_list(request):
-    """List all categories."""
-    if request.method == 'GET':
-        categories = Category.objects.all().order_by('name')
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    elif request.method == 'POST':
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def category_detail(request, pk):
-    """Retrieve categories, then put, patch or delete the category."""
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        return Response({'error': 'Category does not exist'}, status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-    
-    elif request.method == 'PUT':
-        serializer = CategorySerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'PATCH':
-        serializer = CategorySerializer(category, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-@api_view(['GET'])
-def category_roots(request):
-    """Get all root category (category that has no parent)"""
-    roots = Category.objects.filter(parent__isnull=True)
-    serializer = CategorySerializer(roots, many=True)
-    return Response(serializer.data)
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
 
-# === SUPPLIERS VIEWS ===
-@api_view(['GET', 'POST'])
-def supplier_list(request):
-    """List all suppliers of a medicine."""
-    if request.method == 'GET':
-        suppliers = Supplier.objects.all()
-        serializer = SupplierSerializer(suppliers, many=True)
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        serializer = SupplierSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def supplier_detail(request, pk):
-    """Retrieve suppliers of a medicine, then put, patch, or delete the supplier"""
-    try:
-        supplier = Supplier.objects.get(pk=pk)
-    except Supplier.DoesNotExist:
-        return Response({"errors": "Supplier does not exist."}, status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = SupplierSerializer(supplier)
-        return Response(serializer.data)
-    
-    elif request.method == 'PUT':
-        serializer = SupplierSerializer(supplier, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'PATCH':
-        serializer = SupplierSerializer(supplier, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        supplier.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-# === MEDICINES VIEWS  === 
-@api_view(['GET', 'POST'])
-def medicine_list(request):
-    """List all medicines, or create a new medicine."""
-    if request.method == 'GET':
-        medicines = Medicine.objects.all().order_by('created_at')
-        serializer = MedicineSerializer(medicines, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = MedicineSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def medicine_detail(request, pk):
-    """Retrieve medicines, then put, patch, or delete medicine."""
-    # Find or fail
-    try:
-        medicine = Medicine.objects.get(pk=pk)
-    except Medicine.DoesNotExist:
-        return Response({'error': 'Medicine does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-
-    if request.method == 'GET':
-        serializer = MedicineSerializer(medicine)
-        return Response(serializer.data)
-    
-    elif request.method == 'PUT':
-        serializer = MedicineSerializer(medicine, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'PATCH':
-        serializer = MedicineSerializer(medicine, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        medicine.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class MedicineViewSet(viewsets.ModelViewSet):
+    queryset = Medicine.objects.all()
+    serializer_class = MedicineSerializer
