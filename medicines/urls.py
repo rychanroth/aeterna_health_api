e8cb436baeb11718f83d75126d5f9b69e628a29d
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-01 16:22:21 +0700

    feat(url): add supplier-list and supplier-detail

--- medicines/urls.py
+++ medicines/urls.py
@@ -2,10 +2,15 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # Medicines
     path('medicines/', views.medicine_list, name='medicine-list'),
     path('medicines/<int:pk>/', views.medicine_detail, name='medicine-detail'),
 
-    # Category
+    # Categories
     path('categories/', views.category_list, name='category-list'),
-    path('categories/<int:pk>', views.category_detail, name='category-detail')
+    path('categories/<int:pk>', views.category_detail, name='category-detail'),
+
+    # Suppliers
+    path('suppliers/', views.supplier_list, name='supplier-list'),
+    path('suppliers/<int:pk>', views.supplier_detail, name='supplier-detail'),
 ]
