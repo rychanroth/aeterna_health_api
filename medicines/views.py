96d52d9d6194c4a5afff515a826e32b87e6eab11
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-01 17:10:08 +0700

    feat(view): add category_roots

--- medicines/views.py
+++ medicines/views.py
@@ -53,6 +53,13 @@ def category_detail(request, pk):
         category.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+@api_view(['GET'])
+def category_roots(request):
+    """Get all root category (category that has no parent)"""
+    roots = Category.objects.filter(parent__isnull=True)
+    serializer = CategorySerializer(roots, many=True)
+    return Response(serializer.data)
+
 # === SUPPLIERS VIEWS ===
 @api_view(['GET', 'POST'])
 def supplier_list(request):
