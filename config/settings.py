0fa8372480c5984b58c1227670f0bc759fa4e6ca
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-02 18:37:49 +0700

    feat(setting): alter database from sqlite to oracle

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'XEPDB1',
+        'USER': 'aeterna_health',
+        'PASSWORD': 'aub12345', 
+        'HOST': 'localhost',
+        'PORT': 1521
     }
 }
 
