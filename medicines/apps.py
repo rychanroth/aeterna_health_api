f4e766aa7b8c6639b14cbe5d9fe55ada36856581
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-02 16:42:09 +0700

    feat(app): register `signals`

--- medicines/apps.py
+++ medicines/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class MedicinesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'medicines'
+
+    def ready(self):
+        import medicines.signals
