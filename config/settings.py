03c4cd1f4eac46420cb33eee73182ab4911d6bd9
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-03 11:26:23 +0700

    feat(setting): register `AUTH_USER_MODEL`

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'medicines.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
