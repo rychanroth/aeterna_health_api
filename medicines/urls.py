52f371e181c16624c2d937cb154902b009ca8a03
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-03 14:26:18 +0700

    feat(url): add `user` and `sale`

--- medicines/urls.py
+++ medicines/urls.py
@@ -3,9 +3,11 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path
 
 router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
 router.register(r'categories', views.CategoryViewSet, basename='category')
 router.register(r'suppliers', views.SupplierViewSet, basename='supplier')
 router.register(r'medicines', views.MedicineViewSet, basename='medicine')
+router.register(r'sales', views.SaleViewSet, basename='sale')
 
 urlpatterns = [
     path('login/', views.login, name="login")
