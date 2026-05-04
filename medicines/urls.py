211e338dd3fdf85e7d24e8f5af0ae86b511bfff2
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-04 18:55:40 +0700

    feat(url): add `doctor`

--- medicines/urls.py
+++ medicines/urls.py
@@ -8,6 +8,7 @@ router.register(r'categories', views.CategoryViewSet, basename='category')
 router.register(r'suppliers', views.SupplierViewSet, basename='supplier')
 router.register(r'medicines', views.MedicineViewSet, basename='medicine')
 router.register(r'sales', views.SaleViewSet, basename='sale')
+router.register(r'doctors', views.DoctorViewSet, basename='doctor')
 
 urlpatterns = [
     path('login/', views.login, name="login")
