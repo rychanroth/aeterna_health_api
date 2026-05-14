b0c54fa905581fc65a2e82bb1f676ab658172276
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-14 11:48:06 +0700

    change(model): from default role.cashier to role.admin

--- medicines/models.py
+++ medicines/models.py
@@ -15,7 +15,7 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
-        default=Role.CASHIER
+        default=Role.ADMIN
     )
     phone = models.CharField(max_length=20, blank=True)
 
