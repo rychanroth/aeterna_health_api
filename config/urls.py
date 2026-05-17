d50d4eb02b0a0f5077b82aac64dc7bcee8c682a0
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-17 11:19:22 +0700

    feat(url): add for `schema`, `swagger-ui` and `redoc`

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('medicines.urls'))
+    path('api/', include('medicines.urls')),
+
+    # YOUR NEW DOCUMENTATION ROUTES:
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    
+    # Swagger UI (Interactive testing interface)
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # ReDoc (Cleaner, read-only documentation)
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
