e72d582e922ba127639e06ae1ae23ce6535b9d92
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-17 09:02:46 +0700

    security: allow cors = true while debugging

--- config/settings.py
+++ config/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +141,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
