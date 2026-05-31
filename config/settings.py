31561c113461c50f261a4af929dbcf546ab767cb
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-31 08:41:54 +0700

    chore(setting): on `REST_FRAMEWORK` add pagination

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema', 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 SPECTACULAR_SETTINGS = {
