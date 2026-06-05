b2503e54349b33f043fc2360c72d8d1084c05adf
Ry Chanroth <rychanroth2009@gmail.com>
2026-06-05 21:56:19 +0700

    chore: change DATABASE to sqlite

--- config/settings.py
+++ config/settings.py
@@ -82,18 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'localhost:1521/XEPDB1', # provide full connect string here means you'll let `HOST` and `PORT` be empty string
-        'USER': 'aeterna_health',
-        'PASSWORD': 'aub12345', 
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
